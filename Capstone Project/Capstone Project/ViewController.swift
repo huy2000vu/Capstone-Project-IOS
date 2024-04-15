e42be8518eff1d1a117397aa27b023704de0254e
huy2000vu <huy2000vu@gmail.com>
2024-04-15 13:17:55 -0700

    added tableview

---
+++ Capstone Project/Capstone Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capstone Project
+//
+//  Created by X Y on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
